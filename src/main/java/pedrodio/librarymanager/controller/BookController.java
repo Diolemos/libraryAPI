f9d5884ad7ace0f313b56a00da80029278a5ee22
Dio Lemos <pedro.devsc@gmail.com>
2024-07-21 19:44:47 -0300

    Create Controller layer; create BookController create findById and create endpoints in the book controller

---
+++ src/main/java/pedrodio/librarymanager/controller/BookController.java
@@ -0,0 +1,47 @@
+package pedrodio.librarymanager.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import pedrodio.librarymanager.model.Book;
+import pedrodio.librarymanager.service.BookService;
+
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    
+     private final BookService bookService ;
+
+     public BookController(BookService bookService){
+        this.bookService = bookService;
+     }
+
+     @GetMapping("/{id}")
+     public ResponseEntity<Book> findById(@PathVariable Long id){
+        
+        var book = bookService.findById(id);
+
+        return ResponseEntity.ok(book);
+     }
+
+     @PostMapping("")
+     public ResponseEntity<Book> create(@RequestBody Book book){
+        
+        var newBook = bookService.create(book);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(newBook.getId())
+        .toUri();
+        
+        return ResponseEntity.created(location).body(newBook);
+     }
+}
