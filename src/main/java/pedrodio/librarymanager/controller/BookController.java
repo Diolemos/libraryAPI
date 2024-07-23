09fe8cb120c3affb9796a52ad3c0e8b672be925f
Dio Lemos <pedro.devsc@gmail.com>
2024-07-23 11:27:00 -0300

    create update endpoint

--- src/main/java/pedrodio/librarymanager/controller/BookController.java
+++ src/main/java/pedrodio/librarymanager/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -42,4 +43,14 @@ public ResponseEntity<Book> create(@RequestBody Book book) {
         .toUri();
     return ResponseEntity.created(location).body(newBook);
 }
+
+@PutMapping("/{id}")
+public ResponseEntity<Book> update(@PathVariable Long id,  @RequestBody Book book) {
+    var updatedBook = bookService.update(id,book);
+   
+    return ResponseEntity.ok(updatedBook);
+}
+
+
+
 }
